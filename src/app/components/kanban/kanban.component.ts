bcbae9915514ce64e50726eca3af52d3f2b479c1
TahaYasarDev <taha.yasar.dev@outlook.com>
2025-06-07 23:35:40 +0200

    Popup

--- src/app/components/kanban/kanban.component.ts
+++ src/app/components/kanban/kanban.component.ts
@@ -7,28 +7,21 @@ import {
   CdkDrag,
   CdkDropList,
 } from '@angular/cdk/drag-drop';
+import { NgIf, NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-kanban',
   standalone: true,
-  imports: [CdkDropList, CdkDrag],
+  imports: [CdkDropList, CdkDrag, NgIf, NgFor, FormsModule],
   templateUrl: './kanban.component.html',
   styleUrl: './kanban.component.scss',
 })
 export class KanbanComponent {
-  backlog = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-
-  development = [
-    'Get up',
-    'Brush teeth',
-    'Take a shower',
-    'Check e-mail',
-    'Walk dog',
-  ];
-
-  inProgress = ['inProgress', 'PinProgress', 'inProgresse', 'inProgressep'];
-
-  done = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  backlog: Item[] = [];
+  development: Item[] = [];
+  inProgress: Item[] = [];
+  done: Item[] = [];
 
   ngAfterViewInit(): void {
     const editableElements = document.querySelectorAll('[contenteditable]');
@@ -37,7 +30,7 @@ export class KanbanComponent {
     });
   }
 
-  drop(event: CdkDragDrop<string[]>) {
+  drop(event: CdkDragDrop<Item[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(
         event.container.data,
@@ -65,4 +58,41 @@ export class KanbanComponent {
   onAnimationEnd() {
     this.isJumping = false;
   }
+
+  showPopup = false;
+
+  lines: string[] = ['', '', '', ''];
+
+  handleClick() {
+    this.showPopup = true;
+    this.lines = ['', '', '', ''];
+  }
+  newTask: Item = {
+    jira: '',
+    title: '',
+    texte: '',
+    estimate: 1,
+    progress: 0,
+  };
+
+  addTask() {
+    if (this.newTask.title.trim() === '') {
+      alert('Title is required');
+      return;
+    }
+    this.backlog.push({ ...this.newTask }); // on push une copie
+    this.showPopup = false;
+  }
+
+  trackByFn(index: number, item: Item): string {
+    return item.jira;
+  }
+}
+
+interface Item {
+  jira: string;
+  title: string;
+  texte: string;
+  estimate: number;
+  progress: number;
 }
